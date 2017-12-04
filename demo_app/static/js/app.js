bf61efb82afcc5d77d15e1a93632eef97b83747d
oleg.buchko <oleg.justdo@gmail>
2017-12-04 15:26:38 +0200

    Styled xs device

--- demo_app/static/js/app.js
+++ demo_app/static/js/app.js
@@ -18,6 +18,11 @@ define([ 'jquery', 'underscore', 'form', 'switch', 'owl' ], function ( $, _, For
     			nav: true,
 	        	items: 1
 	        });
+
+	        $( '.mobile-menu-btn' ).on( 'click', function () {
+	        	$( '.extra-menu' ).toggleClass( 'active--js' );
+	        })
+
 	        new Switch( { el: '.catalog-menu' } );
 	        new Form({el: 'form'});
 
