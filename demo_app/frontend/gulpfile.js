6c067f98645d6c6135350f112d2dfca5e0045c63
oleg.buchko <pokerext@gmail.com>
2017-12-05 03:12:12 +0200

    Minify css

--- demo_app/frontend/gulpfile.js
+++ demo_app/frontend/gulpfile.js
@@ -4,6 +4,7 @@ var less = require("gulp-less");
 var sourcemaps = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
 var autoprefixer = require("gulp-autoprefixer");
+var minifyCss = require('gulp-clean-css');
 
 var lessSrc = "./less/index.less";
 var cssDest = "../static/css";
@@ -11,14 +12,21 @@ var cssDest = "../static/css";
 gulp.task('less', function() {
     return gulp.src(lessSrc)
         .pipe( sourcemaps.init() )
-        	.pipe(rename('styles.less'))
-        	.pipe(less())
-        	.on('error', function ( err ) {
-        		console.log( err );
-        	})
+            .pipe(rename('styles.less'))
+            .pipe(less())
+            .on('error', function ( err ) {
+                console.log( err );
+            })
         .pipe(sourcemaps.write())
         .pipe(gulp.dest( cssDest ))
         .pipe(livereload());
+});
+
+
+gulp.task('minify-css', () => {
+  return gulp.src( cssDest + '/styles.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest( cssDest ));
 });
 
 gulp.task('dev', function() {
